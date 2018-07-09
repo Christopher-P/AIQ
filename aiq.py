98dfa8419d0902a8e798b2802997ec582eb925d7
Christopher Pereyda <christopher.pereyda@gmail.com>
2018-07-09 00:24:03 -0700

    Beginning vizdoom
    switching to lappy

--- aiq.py
+++ aiq.py
@@ -215,10 +215,29 @@ class RPM(AIQ):
 		return super().connect()
 		
 # Make a ViZDoom class (to be tested on linux system, windows is meh for it)
-#class ViZDoom(AIQ):
+class ViZDoom(AIQ):
+	def __init__(self):
+		super().__init__()
+		try:
+			import ViZDoom
+		except:
+			print("Failed to import ViZDoom, make sure you have ViZDoom installed!")
+			
 	
+	#def get_header(self):
+		
+		
+	def connect(self):
+		return super().connect()
+		
+	def render(self):
+		self.env.render()
 	
-
-#
+	def act(self, action):
+		return super().act(action, 'CartPole-v0')
+		
+	def reset(self):
+		super().reset()
+		self.env.reset()
 
 
