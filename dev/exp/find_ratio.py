422daf945912737edc9e8849db9c79fac3f43d84
Christopher Pereyda <christopher.pereyda@gmail.com>
2019-12-15 17:08:10 -0800

    scaling data

--- dev/exp/find_ratio.py
+++ dev/exp/find_ratio.py
@@ -56,7 +56,14 @@ def record(filename, nameA, nameB, values, values2=None):
 
 data = load_file('RAW.csv')
 
+scales = [(15.0, 261.0), (15.0, 265.0), (-699.0, -615.0), (-399.0, -399.0), (-71.0, 144.0), (0.0, 1.0), (-26058.0, -143.0), (1318.0, 1782.0), (0.0, 1.0), (-1164.0, -162.0)]
+
+counter = 0
 for entry in data:
+    
+    if counter >= 10:
+        counter = 0
+
     # Get names from data
     name_A = entry[0]
     name_B = entry[1]
@@ -68,13 +75,52 @@ for entry in data:
     A_r = history_A['episode_reward']
     B_r = history_B['episode_reward']
 
+    print(name_A, name_B)
+
+    for ind,val in enumerate(A_r):
+        try:
+            A_r[ind] = (val - scales[counter][0]) / (scales[counter][1] - scales[counter][0])
+        except:
+            p = 10
+            
+    for ind,val in enumerate(B_r):
+        try:
+            B_r[ind] = (val - scales[counter][0]) / (scales[counter][1] - scales[counter][0])
+        except:
+            p = 10
+    counter += 1
+
+    try:
+        Ratio = (AuC(B_r) - AuC(A_r)) / AuC(A_r)
+    except Exception as e:
+        #print(A_r)
+        Ratio = 0
+
+    # Record JumpStart
+    # agent_B_init_score - agent_A_init_score
+    JumpStart = B_r[0] - A_r[0]
+    record('JumpStart.csv', name_A, name_B, JumpStart)
+
+    # Record Asymptotic Performance
+    # agent_B_final_score - agent_A_final_score
+    samples_AP = 5                  # Number of samples at end
+    Asymptotic = (sum(B_r[-samples_AP:])/samples_AP) - (sum(A_r[-samples_AP:])/samples_AP)
+    record('Asymptotic.csv', name_A, name_B, Asymptotic)
+
+    # Record Total Reward
+    # Makes more sense to do average (since a lot are time dependent)
+    # sum(A_reward) - sum(B_reward)
+    try:
+        Reward = (sum(B_r) / len(B_r)) - (sum(A_r) / len(A_r))
+    except:
+        Reward = -999999
+    record('Reward.csv', name_A, name_B, Reward)
+
     # Record Transfer Ratio
     # (AuC_B - AuC_A) / AuC_A
     try:
         Ratio = (AuC(B_r) - AuC(A_r)) / AuC(A_r)
-    except Exception as e:
-        print(A_r)
-        Ratio = 0
-
+    except:
+        Ratio = -999999
     record('Ratio.csv', name_A, name_B, Ratio)
 
