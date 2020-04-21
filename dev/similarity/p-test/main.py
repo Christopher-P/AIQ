fe46bd154d2a1e483a838cb6181f8ad4cb6606cb
christopher-p <christopher.pereyda@gmail.com>
2020-04-20 17:18:36 -0700

    run one last time

--- dev/similarity/p-test/main.py
+++ dev/similarity/p-test/main.py
@@ -95,15 +95,14 @@ def main():
 
     ### Running for just comparing to cartpole!
     for ind, val in enumerate(dats):
-        for ind2, val2 in enumerate(dats):
-            print(names[ind], names[ind2])
+        print('CART', names[ind])
 
             # Data comes from np-plot.py
             for i in range(0,11):
                 p = i / 10.0
-                tl.C = tl.join(val, val2, p)
-                results = run_it(val, val2, tl.C)
-                log_it([names[ind], names[ind]]+ [p] + results)
+                tl.C = tl.join(tl.cp, val, p)
+                results = run_it(tl.cp, val, tl.C)
+                log_it('CART', names[ind]]+ [p] + results)
             
     return None
 
