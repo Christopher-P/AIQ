072ca4ccdd307d4bb38ae75e3791e6131093a0a0
christopher-p <christopher.pereyda@gmail.com>
2020-07-19 18:35:01 -0700

    finishing up classifcation plots

--- dev/complexity/classifcation_exp/box.py
+++ dev/complexity/classifcation_exp/box.py
@@ -6,47 +6,43 @@ import math
 auc = [8.802, 8.513, 13.68, 19.466, 1.519]
 mse = [0.62, 0.64, 1.31, 0.75, 1.99]
 
-# Find rmse from mse (functionally equaivalent to STDEV
-rmse = []
-for i in range(5):
-    rmse.append(math.sqrt(mse[i]) / math.sqrt(1000))
-
-# Norm them
-sum_rmse = sum(rmse)
-sum_auc = sum(auc)
-sum_mse = sum(mse)
-for ind in range(5):
-    auc[ind] = auc[ind]/sum_auc
-    mse[ind] = mse[ind]/sum_mse
-    
-#for ind in range(5):
-#    mse[ind] = math.sqrt(mse[ind]) * 10
-
-entropy_n = [182532.4206179811,
+# Generated from entropy.py
+entropy = [182532.4206179811,
 211904.015471518,
 269661.94757943094,
 434866.9281534464,
 50090.15641089917]
 
-e_s = sum(entropy_n)
-for ind, val in enumerate(entropy_n):
-    entropy_n[ind] = val/e_s
-    print(round(entropy_n[ind],4))
+# Normalize everything to sum=1
+sum_mse = sum(mse)
+sum_auc = sum(auc)
+sum_entropy = sum(entropy)
+for ind in range(5):
+    mse[ind] = mse[ind]/sum_mse
+    auc[ind] = auc[ind]/sum_auc
+    entropy[ind] = entropy[ind]/sum_entropy
 
+# Find SE from mse (RMSE functionally equaivalent to STDEV)
+standard_error = []
+for i in range(5):
+    standard_error.append(1.96 * math.sqrt(mse[i]) / math.sqrt(1000))
 
-print(rmse)
-    
 means = np.asarray(auc)
-std = np.asarray(mse)
 
 # create stacked errorbars:
-#plt.errorbar(np.arange(5), means, std, fmt='ok', lw=3)
-plt.errorbar(np.arange(5), means, yerr=[rmse, rmse],
-             fmt='.k', ecolor='gray', lw=1)
-plt.scatter(np.arange(5), entropy_n, color='r', marker=',')
+# Main bars
+plt.errorbar(np.arange(5), means, yerr=[standard_error, standard_error],
+             fmt='.k', ecolor='black', lw=1, capsize=4, capthick=1)
+# Entropy points
+plt.scatter(np.arange(5), entropy, color='r', marker=',')
+
+# Formatting
 plt.legend(['Entropic Prediction', 'Empirical Prediction'])
 plt.xlim(-0.5, 4.5)
 plt.xticks(np.arange(5), ["MNIST", "FMNIST", "C10", "C100", "CARTPOLE"])
 plt.ylim(0, 0.5)
+plt.xlabel('Domains')
+plt.ylabel('Complexity')
 
-plt.show()
+# Save to file
+plt.savefig('Entropies.png')
